133744f414b5a3ad7646956bbe0bae264eaea9e4
Lee Glendenning <glen9190@mylaurier.ca>
2017-04-08 21:02:42 -0400

    Decision tree model successfully being created with CART and written to file

--- src/com/dmec/dtree/Dmec.java
+++ src/com/dmec/dtree/Dmec.java
@@ -1,199 +1,49 @@
 package com.dmec.dtree;
 
 public class Dmec {
-    
+
     static int numClusters = 3;
     static double mins = 0.5;
-    
+    static CARTandID3 cart = null;
+
     public static void main(String[] args) {
-        
-        String trainingFilePath = null;
-        String testingFilePath;
-        String outputFilePath;
-        
-        int index_a = -1;
-        int index_t = -1;
-        int index_T = -1;
-        int index_o = -1;
-        
-        for (int i = 0; i < args.length; i += 2) {
-        	//System.out.println("args[" + i + "] = " + args[i]);
-            switch(args[i]) {
-                case "-a":
-                    index_a = i;
-                    break;
-                case "-t":
-                    index_t = i;
-                    break;
-                case "-T":
-                    index_T = i;
-                    break;
-                case "-o":
-                    index_o = i;
-                    break;
-                default:
-                    printUsage();
-                    System.exit(1);
-                    break;
-            }
-        }
-        
-        
-        // debug, print arguments
-        //System.out.println(Arrays.toString(args));
-        /*System.out.println("index_a: " + index_a);
-        System.out.println("index_t: " + index_t);
-        System.out.println("index_T: " + index_T);
-        System.out.println("index_o: " + index_o);*/
-        
-        // if no alg or test file or output path specified then invalid. if alg is 1 or 2 and there is no training file then invalid
-        if (index_a == -1 || index_T == -1 || index_o == -1 || (index_t == -1 && (args[index_a+1].equals("1") || args[index_a+1].equals("2")))) {
-            printUsage();
-            System.exit(1);
-        }
-        
-        // Make use of training file
-        if (index_t != 0) {
-            trainingFilePath = args[index_t + 1];
-        } else {
-            System.err.println("No training file provided");
-            System.exit(1);
-        }
-        
-        // Make use of testing file
-        testingFilePath = args[index_T + 1];
-        
-        // Make use of output file
-        outputFilePath = args [index_o + 1];
-        
-        // Call specified algorithm
-        // 1 - CART, 2 - ID3,  3 - k-mean, 4 - Apriori
-        switch (Integer.parseInt(args[index_a + 1])) {
 
-            case 1:
-                doCART(trainingFilePath, testingFilePath, outputFilePath);
-                break;
+        String trainingFilePath = "C:\\Users\\The Boss\\Documents\\GitHub\\Data-Mining-and-Enterprise-Computing\\test\\data2.txt"; // THIS WILL BE SERVER SIDE
+        String testingFilePath = "";
+        String outputFilePath = "output";
 
-            case 2:
-                doID3(trainingFilePath, testingFilePath, outputFilePath);
-                break;
-
-            case 3:
-                //doKMeans(testingFilePath);
-                break;
-
-            case 4:
-                //doApriori(testingFilePath);
-                break;
-
-            default:
-                System.out.println("Invalid algorithm number.");
-                System.exit(1);
-                break;
-        }
+        doCART(trainingFilePath, outputFilePath);
         
-        
-        
+        //doCARTPrediction(testingFilePath);
     }
-    
-    static void printUsage() {
-        System.out.println("Usage: -a <alg-#> -t <training-file-path> -T <testing-file-path> -o <output-model-file-path>");
-    }
-    
+
     /**
-     * Expects file to by comma (,) delimited
-     * 
-     * @param testingFilePath
-     */
-   /* public static void doKMeans(String testingFilePath) {
-        KMeans km;
-        try {
-            km = new KMeans(testingFilePath, numClusters);
-            km.cluster();
-        } catch (IOException ex) {
-            System.err.println("File not found");
-        }
-    }*/
-    
-    /**
-     * Expects file to be space ( ) delimited
-     * 
-     * @param testingFilePath
-     */
-    /*public static void doApriori(String testingFilePath) {
-        Apriori a;
-        try {
-            a = new Apriori(testingFilePath, mins);
-            a.associationMine();
-        } catch (IOException ex) {
-            System.err.println("File not found");
-        }
-    }*/
-    
-    /**
-     * Expects comma delimited file with first row being classifying attribute and second being list of attributes
-     * 
+     * Expects comma delimited file with first row being classifying attribute
+     * and second being list of attributes
+     *
      * @param trainingFilePath
-     * @param testingFilePath
+     * @param outputFilePath
      */
-    public static void doCART(String trainingFilePath, String testingFilePath, String outputFilePath) {
-    	CARTandID3 cart = null;
-        
-		try {
-			DataSet trainingDataSet = new DataSet(trainingFilePath, ",", true);
-			cart = new CARTandID3(trainingDataSet, "CART", outputFilePath);
-            
-		} catch (Exception ex) {
-			System.err.println("Failed creating model with training set");
+    public static void doCART(String trainingFilePath, String outputFilePath) {
+        try {
+            DataSet trainingDataSet = new DataSet(trainingFilePath, ",", true);
+            cart = new CARTandID3(trainingDataSet, "CART", outputFilePath);
+
+        } catch (Exception ex) {
+            System.err.println("Failed creating model with training set");
             System.exit(1);
-		}
-        
-		try {
-			DataSet testDataSet = new DataSet(testingFilePath, ",", false);
-	        DataSet classifiedDataSet1 = cart.classify(testDataSet);
-	        classifiedDataSet1.printDataSet();
-		} catch (Exception ex) {
-			System.err.println("Failed classifying test set");
+        }
+    }
+
+    public static void doCARTPrediction(String testingFilePath) {
+        try {
+            DataSet testDataSet = new DataSet(testingFilePath, ",", false);
+            DataSet classifiedDataSet1 = cart.classify(testDataSet);
+            classifiedDataSet1.printDataSet();
+        } catch (Exception ex) {
+            System.err.println("Failed classifying test set");
             System.exit(1);
-		}
+        }
     }
-    
-    public static void doID3(String trainingFilePath, String testingFilePath, String outputFilePath) {
-    	CARTandID3 id3 = null;
-        
-		try {
-			DataSet trainingDataSet = new DataSet(trainingFilePath, ",", true);
-			id3 = new CARTandID3(trainingDataSet, "ID3", outputFilePath);
-            
-		} catch (Exception ex) {
-			System.err.println("Failed creating model with training set");
-            System.exit(1);
-		}
-        
-		try {
-			DataSet testDataSet = new DataSet(testingFilePath, ",", false);
-	        DataSet classifiedDataSet1 = id3.classify(testDataSet);
-	        classifiedDataSet1.printDataSet();
-		} catch (Exception ex) {
-			System.err.println("Failed classifying test set");
-            System.exit(1);
-		}
-    }
-    
-    public static void doCARTPrediction(String testingFile) {
-    	
-    }
-    
-    public static void doID3Prediction(String testingFile) {
-	    	
-    }
-	
-	/*public static void doKMeansPrediction(String testingFile) {
-		
-	}
-	
-	public static void doAprioriPrediction(String testingFile) {
-		
-	}*/
-    
+
 }
