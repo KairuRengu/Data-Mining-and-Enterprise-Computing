61029967859ca1a6a392ad426d44f3dd5360ae8b
Lee Glendenning <glen9190@mylaurier.ca>
2017-04-09 15:47:10 -0400

    Add player scoring

--- src/com/dmec/dtree/CARTandID3.java
+++ src/com/dmec/dtree/CARTandID3.java
@@ -20,9 +20,9 @@ public class CARTandID3 {
     private final DecisionTree dTree;
     private final String alg;
     private Writer globalWriter;
-    
+
     /**
-     * 
+     *
      * @param d - data set to learn from
      * @param outputFilePath - path to file generated
      * @param alg - supports "CART" and "ID3
@@ -34,23 +34,23 @@ public class CARTandID3 {
         this.dTree = new DecisionTree();
         this.alg = alg;
         try {
-			this.globalWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFilePath), "utf-8"));
-		} catch (IOException e) {
-			System.err.println("Failed to create output model file");
-			System.exit(1);
-		}
+            this.globalWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outputFilePath), "utf-8"));
+        } catch (IOException e) {
+            System.err.println("Failed to create output model file");
+            System.exit(1);
+        }
         constructDecisionTree();
     }
 
     private void constructDecisionTree() {
-        
+
         while (!this.data.getDataSet().isEmpty()) {
             System.out.println("***");
             HashMap<String, Double> splitValues = new HashMap();
-        
+
             // HashMap<String, HashMap<Object, HashMap<Object, Integer>>> - <attrName, <attrValue, <class-value, #-occurrences>>>
             HashMap<String, HashMap<Object, HashMap<Object, Integer>>> classDistributionSet = new HashMap();
-        
+
             // Calculate GINIsplit for each attribute of dataSet
             for (String attrName : this.attributeNames) {
                 if (!attrName.equals(this.className)) { // skip the class attribute
@@ -72,14 +72,14 @@ public class CARTandID3 {
                 }
             }
             // 3. take smallest GINISplit value as node => remove from this.attributeNames
-            
+
             String nodeAttrName = null;
             if (this.alg.equals("CART")) {
                 nodeAttrName = determineMinSplitGINIAttr(splitValues); // Get name of attribute that yielded the smalled GINIsplit value
             } else if (this.alg.equals("ID3")) {
                 nodeAttrName = determineMaxSplitGAINAttr(splitValues);
             } else {
-            	System.err.println("Algorithm requested is not recognized");
+                System.err.println("Algorithm requested is not recognized");
                 System.exit(1);
             }
 
@@ -93,62 +93,62 @@ public class CARTandID3 {
                     break;
                 }
             }
-            
+
             System.out.println("Adding node to tree: " + nodeAttrName);
             addNodeToDecisionTree(nodeAttrName, minClassDist); // Also removes necessary rows from data set for the next branch
             System.out.println("***\n\n\n");
-            
+
             if (nodeAttrName == null) {
                 break;
             }
         }
-        
+
         System.out.println("\n\nDecision tree successfully constructed.\n");
-        
-        
+
         genOutputFile(this.dTree.getRoot(), "null");
         try {
-			this.globalWriter.close();
-		} catch (IOException e) {
-			// no problem
-		}
+            this.globalWriter.close();
+        } catch (IOException e) {
+            // no problem
+        }
     }
-    
+
     public DataSet classify(DataSet d) {
         return this.dTree.classify(d, this.className);
     }
-    
+
     /**
-     * 
-     * @param nodeAttrName - String of attribute name that will be a node in the decision tree
-     * @param minClassDist - HashMap<Object, HashMap<Object, Integer>> - <attrValue, <class-value, #-occurrences>>
+     *
+     * @param nodeAttrName - String of attribute name that will be a node in the
+     * decision tree
+     * @param minClassDist - HashMap<Object, HashMap<Object, Integer>> -
+     * <attrValue, <class-value, #-occurrences>>
      */
     private void addNodeToDecisionTree(String nodeAttrName, HashMap<Object, HashMap<Object, Integer>> minClassDist) {
         //System.out.println("Adding node " + nodeAttrName);
         this.dTree.addNode(nodeAttrName, false); // false because this is an attribute, not a classifier
-        
+
         if (nodeAttrName == null) {
             return;
         }
-        
+
         //System.out.print("\"" + nodeAttrName + "\" removed from this.attributeNames. Size " + this.attributeNames.size() + " -> ");
         this.attributeNames.remove(nodeAttrName);
         //System.out.println(this.attributeNames.size());
-        
-        
+
         // Add leaf(s) if exists
         HashMap<Object, Object> stagedLeaf = new HashMap();
         // this if statement checks if there should be leaf node(s) created and gets the attrName for it/them
         if ((stagedLeaf = this.data.getStagedLeafValue(nodeAttrName)) != null) {
             for (Map.Entry<Object, Object> leaf : stagedLeaf.entrySet()) {
                 //System.out.println("Adding leaf " + (String)leaf.getKey() + ", " + (String)leaf.getValue());
-                this.dTree.getCurNode().addChild(leaf.getKey(), (String)leaf.getValue(), true, false); //addChild(attrValue, classValue, thisIsAClassifier, takeBranch?)
+                this.dTree.getCurNode().addChild(leaf.getKey(), (String) leaf.getValue(), true, false); //addChild(attrValue, classValue, thisIsAClassifier, takeBranch?)
                 updateDataSetForLeafNode(nodeAttrName, leaf.getKey());
             }
         }
-        
+
         this.data.clearStagedLeafs();
-        
+
         // Add remaining branch(es)
         for (Map.Entry<Object, HashMap<Object, Integer>> entry : minClassDist.entrySet()) {
             // if this branchValue has not already been added as a leaf child, add as an empty branch
@@ -159,85 +159,87 @@ public class CARTandID3 {
             }
         }
     }
-    
+
     /**
-     * Removes all rows from dataSet that contain the node name and branch value given
-     * 
+     * Removes all rows from dataSet that contain the node name and branch value
+     * given
+     *
      * @param attrName - node name
      * @param attrValue - branch value
      */
     private void updateDataSetForLeafNode(String attrName, Object attrValue) {
         // Iterate through the data set one row(HashMap<String, Object>) at a time
-        for (int i = 0; i < this.data.getDataSet().size(); i ++) {
-            
+        for (int i = 0; i < this.data.getDataSet().size(); i++) {
+
             // If the row has an occurrence of given <attrName, !attrValue>, remove the row. I.e. does not contain the branch value
             if (this.data.getDataRow(i).containsKey(attrName) && data.getDataRow(i).get(attrName).equals(attrValue)) {
                 //System.out.println("Removing row " + i + " because it contains <" + attrName + ", " + data.getDataRow(i).get(attrName) + ">");
                 this.data.getDataSet().remove(i);
-                i --;
+                i--;
             }
         }
-        
+
         //System.out.println();
         this.data.printDataSet();
     }
-    
+
     /**
-     * 
-     * @param splitGINIValues - HashMap<String, Double> keyed by attr name, GINIsplit value
+     *
+     * @param splitGINIValues - HashMap<String, Double> keyed by attr name,
+     * GINIsplit value
      * @return String attribute name having smallest GINIsplit value
      */
     private String determineMinSplitGINIAttr(HashMap<String, Double> splitGINIValues) {
         SimpleEntry minAttr = null;
-        
+
         for (Map.Entry<String, Double> entry : splitGINIValues.entrySet()) {
-            if (minAttr == null || entry.getValue() < (Double)minAttr.getValue()) {
+            if (minAttr == null || entry.getValue() < (Double) minAttr.getValue()) {
                 //System.out.println("New min: " + entry.getKey() + " : " + entry.getValue());
                 minAttr = new SimpleEntry(entry.getKey(), entry.getValue());
             }
         }
-        
+
         if (minAttr == null) {
             return null;
         }
-        
+
         //System.out.println("Min GINI split is " + minAttr.getKey() + " : " + minAttr.getValue());
-        
-        return (String)minAttr.getKey();
+        return (String) minAttr.getKey();
     }
-    
+
     /**
-     * 
-     * @param splitGAINValues - HashMap<String, Double> keyed by attr name, GAINsplit value
+     *
+     * @param splitGAINValues - HashMap<String, Double> keyed by attr name,
+     * GAINsplit value
      * @return String attribute name having largest GAINsplit value
      */
     private String determineMaxSplitGAINAttr(HashMap<String, Double> splitGAINValues) {
         SimpleEntry maxAttr = null;
-        
+
         for (Map.Entry<String, Double> entry : splitGAINValues.entrySet()) {
-            if (maxAttr == null || entry.getValue() > (Double)maxAttr.getValue()) {
+            if (maxAttr == null || entry.getValue() > (Double) maxAttr.getValue()) {
                 //System.out.println("New min: " + entry.getKey() + " : " + entry.getValue());
                 maxAttr = new SimpleEntry(entry.getKey(), entry.getValue());
             }
         }
-        
+
         if (maxAttr == null) {
             return null;
         }
-        
+
         //System.out.println("Max GAIN split is " + maxAttr.getKey() + " : " + maxAttr.getValue());
-        
-        return (String)maxAttr.getKey();
+        return (String) maxAttr.getKey();
     }
-    
+
     private void printClassDist(HashMap<Object, HashMap<Object, Integer>> classDist) {
         // Consider implementing this
     }
-    
+
     /**
-     * 
+     *
      * @param attrName
-     * @return HashMap<Object, HashMap<String, Integer>> - <attrValue, <class-value, #-occurrences>>
+     * @return HashMap<Object, HashMap<String, Integer>> - <attrValue,
+     * <class-value, #-occurrences>>
      */
     public HashMap<Object, HashMap<Object, Integer>> findClassDist(String attrName) {
         HashMap<Object, HashMap<Object, Integer>> classDist = new HashMap();
@@ -252,13 +254,12 @@ public class CARTandID3 {
                 //System.out.println("Adding new HashMap for " + row.get(attrName));
                 classDist.put(row.get(attrName), new HashMap<Object, Integer>());
             }
-            
+
             // get current count for current row's class value (e.g. if Round has been found once for the given attribute, increment its count)
-            if ((occurrences = classDist.get(row.get(attrName)).get(row.get(this.className))) != null){
+            if ((occurrences = classDist.get(row.get(attrName)).get(row.get(this.className))) != null) {
                 //System.out.println("Incrementing " + row.get(attrName) + ">" + row.get(this.className) + " = " + (occurrences + 1));
                 classDist.get(row.get(attrName)).replace(row.get(this.className), occurrences + 1); // increment class value occurrences for the attribute value. e.g. for attribute value A, increment Round by 1
-            } 
-            else { // no entry yet, add class value as key and empty HashMap as value. E.g. Round, 0
+            } else { // no entry yet, add class value as key and empty HashMap as value. E.g. Round, 0
                 //System.out.println("Adding new HashMap for " + row.get(attrName) + ">" + row.get(this.className));
                 classDist.get(row.get(attrName)).put(row.get(this.className), 1);
             }
@@ -266,17 +267,18 @@ public class CARTandID3 {
         }
         return classDist;
     }
-    
 
     /**
-     * 
-     * @param classDist - HashMap<Object, HashMap<String, Integer>> - <attrValue, <class-value, #-occurrences>>
-     * @return double GINI split value for the attribute in the classDist
+     *
+     * @param classDist - HashMap<Object, HashMap<String, Integer>> -
+     * <attrValue, <class-value, #-occurrences>>
+     * @ret
+     * urn double GINI split value for the attribute in the classDist
      */
     private double GINISplit(HashMap<Object, HashMap<Object, Integer>> classDist, String attrName) {
         int totalValues = getTotalValuesInClassDist(classDist);
         double splitGINIValue = 0;
-        
+
         // For each attribute value. E.g. A
         for (Map.Entry<Object, HashMap<Object, Integer>> attr : classDist.entrySet()) {
             int totalForAttr = 0;
@@ -284,7 +286,7 @@ public class CARTandID3 {
             //System.out.println("Counting for " + attr.getKey());
             for (Map.Entry<Object, Integer> entry : classDist.get(attr.getKey()).entrySet()) {
                 if (classValue == null) { // Store class value in case the GINI for the attribute value is 0 meaning it is a leaf
-                    classValue = (String)entry.getKey();
+                    classValue = (String) entry.getKey();
                 }
                 //System.out.println("Counting " + entry.getValue() + " for " + entry.getKey());
                 totalForAttr += entry.getValue();
@@ -293,29 +295,29 @@ public class CARTandID3 {
             //System.out.print("GINI(" + attr.getKey() + ") = 1");
             for (Map.Entry<Object, Integer> entry : classDist.get(attr.getKey()).entrySet()) {
                 //System.out.print(" - (" + entry.getValue() + " / " + totalForAttr + ")^2");
-                
+
                 curGINI -= Math.pow((new Double(entry.getValue()) / new Double(totalForAttr)), 2);
             }
             //System.out.println(" = " + curGINI);
-            
+
             if (doubleIsZero(curGINI)) {
                 //System.out.println("GINI = 0. Adding to stagedLeafs(" + attrName + ", " + attr.getKey() + ", " + classValue + ")");
                 this.data.stageLeaf(attrName, attr.getKey(), classValue);
             }
-            
+
             splitGINIValue += (new Double(totalForAttr) / new Double(totalValues)) * curGINI;
             //System.out.println("GINIsplit += (" + totalForAttr + " / " + totalValues + ") * " + curGINI + " = " + ((new Double(totalForAttr) / new Double(totalValues)) * curGINI));
         }
-        
+
         //System.out.println("GINI split value for attr = " + splitGINIValue);
         return splitGINIValue;
     }
-    
+
     private double GAINSplit(HashMap<Object, HashMap<Object, Integer>> classDist, String attrName) {
         int totalValues = getTotalValuesInClassDist(classDist);
         double totalEntropy = 0.0;
         double classDistEntropy = 0.0;
-        
+
         // For each attribute value. E.g. A
         for (Map.Entry<Object, HashMap<Object, Integer>> attr : classDist.entrySet()) {
             int totalForAttr = 0;
@@ -323,7 +325,7 @@ public class CARTandID3 {
             //System.out.println("Counting for " + attr.getKey());
             for (Map.Entry<Object, Integer> entry : classDist.get(attr.getKey()).entrySet()) {
                 if (classValue == null) { // Store class value in case the Entropy for the attribute value is 0 meaning it is a leaf
-                    classValue = (String)entry.getKey();
+                    classValue = (String) entry.getKey();
                 }
                 //System.out.println("Counting " + entry.getValue() + " for " + entry.getKey());
                 totalForAttr += entry.getValue();
@@ -336,47 +338,48 @@ public class CARTandID3 {
                 curEntropy -= (new Double(entry.getValue()) / new Double(totalForAttr)) * (Math.log10(new Double(entry.getValue()) / new Double(totalForAttr)) / Math.log10(2.));
             }
             //System.out.println(" = " + curEntropy);
-            
+
             if (doubleIsZero(curEntropy)) {
                 //System.out.println("GAIN = 0. Adding to stagedLeafs(" + attrName + ", " + attr.getKey() + ", " + classValue + ")");
                 this.data.stageLeaf(attrName, attr.getKey(), classValue);
             }
-            
+
             totalEntropy += (new Double(totalForAttr) / new Double(totalValues)) * curEntropy;
             //System.out.println("GINIsplit += (" + totalForAttr + " / " + totalValues + ") * " + curGINI + " = " + ((new Double(totalForAttr) / new Double(totalValues)) * curGINI));
-        
+
             classDistEntropy -= (new Double(totalForAttr) / new Double(totalValues)) * (Math.log10(new Double(totalForAttr) / new Double(totalValues)) / Math.log10(2.));
-            
+
         }
-        
+
         //System.out.println("Total Entropy value for attr = " + totalEntropy);
         //System.out.println("ClassDist Entropy value for attr = " + classDistEntropy);
-        
         double splitGAINValue = classDistEntropy - totalEntropy;
         //System.out.println("SplitGAIN value for attr = " + splitGAINValue);
-        
+
         return splitGAINValue;
     }
-    
+
     /**
      * Compares the given double to 0 within a small threshold
-     * 
+     *
      * @param value - double to compare to 0ish
      * @return true if double is within threshold, false otherwise
      */
-    private boolean doubleIsZero(double value){
+    private boolean doubleIsZero(double value) {
         double threshold = 0.0001;
         return value >= -threshold && value <= threshold;
     }
-    
+
     /**
-     * 
-     * @param classDist - HashMap<Object, HashMap<String, Integer>> - <attrValue, <class-value, #-occurrences>>
-     * @return integer total number of values for attribute in classDist
+     *
+     * @param classDist - HashMap<Object, HashMap<String, Integer>> -
+     * <attrValue, <class-value, #-occurrences>>
+     * @ret
+     * urn integer total number of values for attribute in classDist
      */
     private int getTotalValuesInClassDist(HashMap<Object, HashMap<Object, Integer>> classDist) {
         int total = 0;
-        
+
         // For each attribute value. E.g. A
         for (Map.Entry<Object, HashMap<Object, Integer>> attr : classDist.entrySet()) {
             for (Map.Entry<Object, Integer> entry : classDist.get(attr.getKey()).entrySet()) {
@@ -386,40 +389,37 @@ public class CARTandID3 {
         //System.out.println("Total = " + total);
         return total;
     }
-    
+
     /**
      * performs pre-order traversal and writes to output file
-     * 
+     *
      * @param curNode
      * @throws UnsupportedEncodingException
      * @throws FileNotFoundException
      * @throws IOException
      */
     private void genOutputFile(Node curNode, Object branchValue) {
-    	
-		try {
-    		if(curNode == null) {
-    			return;
-    		}
-    		
-    		// Write node value
-    		System.out.println(curNode.getNodeValue());
-    		if (curNode.getNodeValue() == null) {
-    			globalWriter.write("null," + branchValue + "\n");
-    		} else {
-    			globalWriter.write(curNode.getNodeValue() + "," + branchValue + "," + curNode.isClassifer() + "\n");
-    		}
-			
-    		genOutputFile(curNode.getLeftNode(), curNode.getLeftBranchValue());
-    		genOutputFile(curNode.getRightNode(), curNode.getRightBranchValue());
-    		  
-    		  
-		     
-		} catch (IOException e) {
-			System.err.println("Failed while creating output file for model");
-			System.exit(1);
-		}
-    	
-    	
+
+        try {
+            if (curNode == null) {
+                return;
+            }
+
+            // Write node value
+            System.out.println(curNode.getNodeValue());
+            if (curNode.getNodeValue() == null) {
+                globalWriter.write("null," + branchValue + "\n");
+            } else {
+                globalWriter.write(curNode.getNodeValue() + "," + branchValue + "," + curNode.isClassifer() + "\n");
+            }
+
+            genOutputFile(curNode.getLeftNode(), curNode.getLeftBranchValue());
+            genOutputFile(curNode.getRightNode(), curNode.getRightBranchValue());
+
+        } catch (IOException e) {
+            System.err.println("Failed while creating output file for model");
+            System.exit(1);
+        }
+
     }
 }
--- src/com/dmec/dtree/Dmec.java
+++ src/com/dmec/dtree/Dmec.java
@@ -1,9 +1,11 @@
 package com.dmec.dtree;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.HashMap;
+
 public class Dmec {
 
-    static int numClusters = 3;
-    static double mins = 0.5;
     static CARTandID3 cart = null;
 
     public static void main(String[] args) {
@@ -11,10 +13,79 @@ public class Dmec {
         String trainingFilePath = "C:\\Users\\The Boss\\Documents\\GitHub\\Data-Mining-and-Enterprise-Computing\\test\\data2_train.txt"; // THIS WILL BE SERVER SIDE
         String testingFilePath = "C:\\Users\\The Boss\\Documents\\GitHub\\Data-Mining-and-Enterprise-Computing\\test\\data2_test.txt";
         String outputFilePath = "output";
-
+        
+        genTrainingFile("Raptors", "Bulls");
+        
         //doCART(trainingFilePath, outputFilePath);
 
-        doCARTPrediction(testingFilePath, outputFilePath);
+        //doCARTPrediction(testingFilePath, outputFilePath);
+    }
+    
+    public static void genTrainingFile(String team1, String team2) {
+        File[] files1 = getAllFiles(team1);
+        File[] files2 = getAllFiles(team2);
+        
+        
+    }
+    
+    /**
+     * 
+     * @param gameFiles
+     * @return hashmap of data for 5 best players
+     */
+    private static HashMap<String,String>[] getFiveBestPlayers(File[] gameFiles) {
+        HashMap<String,Object>[] bestPlayers = new HashMap[5];
+        
+        
+        
+        return bestPlayers;
+    }
+    
+    /**
+     * 
+     * @param player
+     * @return player score based on weighted statistics of past games
+     */
+    private static double scorePlayer(HashMap<String,String> player) {
+        double score = 0;
+        
+        //name,playerNumber,minutesPlayed,points,+/-,fieldGoalPercent,rebounds,team
+        score += Double.parseDouble(player.get("points"));
+        score += Double.parseDouble(player.get("+/-"));
+        score += Double.parseDouble(player.get("rebounds"));
+        score *= Double.parseDouble(player.get("fieldGoalPercent"));
+        score /= minsToSeconds(player.get("minutesPlayed"));
+        System.out.println("Score for " + player.get("name") + ": " + score);
+        return score;
+    }
+    
+    /**
+     * converts <mm:ss> string to integer of seconds
+     * @param minsString
+     * @return 
+     */
+    private static double minsToSeconds(String minsString) {
+        return Double.parseDouble(minsString.split(":")[1]) + 60 * Integer.parseInt(minsString.split(":")[0]);
+    }
+    
+    /**
+     * 
+     * @param teamName
+     * @return list of paths to files where the given team played
+     */
+    private static File[] getAllFiles(String teamName) {
+        File dir = new File("C:\\Users\\The Boss\\Documents\\GitHub\\Data-Mining-and-Enterprise-Computing\\test\\scraped");
+        File[] files = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.contains(teamName);
+            }
+        });
+
+        /*for (File fileName : files) {
+            System.out.println(fileName);
+        }*/
+        return files;
     }
 
     /**
