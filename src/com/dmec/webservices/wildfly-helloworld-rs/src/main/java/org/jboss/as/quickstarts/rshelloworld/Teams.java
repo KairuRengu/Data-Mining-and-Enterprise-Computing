13cbff2feec1afcbc0236f8c397d7ff9cecf100b
John Norry <ivannorry@gmail.com>
2017-04-10 10:06:56 -0400

    Add files via upload
    REST webservices

---
+++ src/com/dmec/webservices/wildfly-helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/Teams.java
@@ -0,0 +1,24 @@
+package org.jboss.as.quickstarts.rshelloworld;
+
+public class Teams {
+	private String teamA;
+	
+	private String teamB;
+
+	public String getTeamA(){
+		return teamA;
+	}
+	
+	public void setTeamA(String teamA){
+		this.teamA = teamA;
+	}
+	
+	public String getTeamB(){
+		return teamB;
+	}
+	
+	
+	public void setTeamB(String teamB){
+		this.teamB = teamB;
+	}
+}
